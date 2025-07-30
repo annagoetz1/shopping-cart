0eeb5264cabc0d2de4a3678839fd523d083ee6f8
annagoetz1 <annalgoetz1@gmail.com>
2025-07-30 14:17:50 -0400

    Render shopping cart

--- src/App.js
+++ src/App.js
@@ -19,6 +19,18 @@ const onAddToCart = (product) => {
   });
 };
 
+{cartItems.length > 0 && (
+  <div>
+    <h2>Shopping Cart</h2>
+    {cartItems.map((item) => (
+      <div key={item.id}>
+        <p>{item.title} x {item.quantity}</p>
+      </div>
+    ))}
+  </div>
+)}
+
+
 function App() {
   const [products, setProducts] = useState([]);
 
