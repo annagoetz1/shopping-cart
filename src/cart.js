4607c56db77d1b9b1e49c6204dd933ab30a90f5b
annagoetz1 <annalgoetz1@gmail.com>
2025-07-31 19:47:54 -0400

    Import useCart context

--- src/cart.js
+++ src/cart.js
@@ -1,23 +1,27 @@
-import React from React;
+import React from "react";
 import { useState } from "react";
+import { useCart } from './CartContext';
 
-function Cart ({cartItems}) {
-     
-return (
-cartItems.length > 0 && (
-    <div className="cart"style={{ border: "2px solid red", padding: "1rem", marginTop: "2rem" }}>
-     
-      <h2>Shopping Cart</h2>
-      {cartItems.map((item) => (
-        <div key={item.id}>
-          <p>
-            {item.title} x {item.quantity}
-          </p>
-          </div>
-            ))}
-          </div>
-        ))}
 
-        export default Cart;
-    
-  
+function Cart() {
+  const { cartItems } = useCart();
+
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map(item => (
+            <li key={item.id}>
+              {item.title} - Quantity: {item.quantity}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Cart;
