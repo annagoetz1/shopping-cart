97364aefa5badcc3306db824f9e7ac4ae696948c
annagoetz1 <annalgoetz1@gmail.com>
2025-07-31 10:57:21 -0400

    Import browser router, link all layouts for routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import AddToCartButton from './components/AddToCartButton';
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Layout from './Layout';
+import App from './App'; // this is your Products page
+import Cart from './Cart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, // includes NavBar
+    children: [
+      { index: true, element: <App /> }, // default = Products
+      { path: 'cart', element: <Cart /> }, // Cart route
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
