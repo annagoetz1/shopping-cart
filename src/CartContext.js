be7b0f3c588f70ebb2e9e23068e0df6dcd96eb66
annagoetz1 <annalgoetz1@gmail.com>
2025-08-03 18:11:05 -0400

    Add CartContext

---
+++ src/CartContext.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAddToCart = (product) => {
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevItems, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, onAddToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
